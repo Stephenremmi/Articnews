7d8aa01b5fe53062fb0733aa77a878ef0da59d9c
stephen remmi <stephenremmi@gmail.com>
2020-05-31 20:11:52 +0300

    tweak code in app/main/erroy.py and app/main/__init__.py

--- app/main/error.py
+++ app/main/error.py
@@ -1,5 +1,5 @@
 from flask import render_template
-
+from . import main
 @main.errorhandler(404)
 def four_Oh_four(error):
     '''
--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template,request,redirect,url_for
+from . import main
 from flask import render_template,request,redirect,url_for
 from ..request import get_source,article_source,get_category,get_headlines
 
